f42f3f30149961e03475df7869ebb01ed626ecec
John <john.19col@gmail.com>
2019-10-28 12:00:46 -0500

    Add google map to page

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import { User } from './User';
 import { Company } from './Company';
 
-const user: Element = document.createElement('pre');
-user.textContent = JSON.stringify(new User(), undefined, ' ');
-
-const company: Element = document.createElement('pre');
-company.textContent = JSON.stringify(new Company(), undefined, ' ');
-
-document.body.appendChild(user);
-document.body.appendChild(company);
+const settings: google.maps.MapOptions = {
+  zoom: 6,
+  center: { lat: 3, lng: -73 }
+};
+const container: Element = document.getElementById('map');
+const map: google.maps.Map = new google.maps.Map(container, settings);
